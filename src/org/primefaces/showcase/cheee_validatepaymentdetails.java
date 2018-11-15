2efb29e91ce18a318337f6fd176a38c92363ca70
Cheeefang <173976P@mymail.nyp.edu.sg>
2018-11-15 13:02:26 +0800

    efesfsefesf
    wBr

--- src/org/primefaces/showcase/cheee_validatepaymentdetails.java
+++ src/org/primefaces/showcase/cheee_validatepaymentdetails.java
@@ -1,4 +1,34 @@
 package org.primefaces.showcase;
 
+import javax.faces.bean.ManagedBean;
+@ManagedBean
 public class cheee_validatepaymentdetails {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
